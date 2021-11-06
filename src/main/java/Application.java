0caad2c8fdaa978480e2370ce7b7c0fced62b224
Aires <joaoaires98@gmail.com>
2021-11-06 14:02:00 +0000

    update

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -11,7 +11,5 @@ public class Application {
     public static void main(String[] args) {
        Game g = new Game();
        g.run();
-
-
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,19 +1,41 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.security.Key;
 
 public class Game {
-    static Screen screen;
-    Game(){
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
+
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowLeft -> {
+                x -= 1;
+            }
+            case ArrowRight -> {
+                x += 1;
+            }
+            case ArrowUp -> {
+                y -= 1;
+            }
+            case ArrowDown -> {
+                y += 1;
+            }
+        }
+    }
+
+    Game() {
         try {
             TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
-                    .setInitialTerminalSize(terminalSize);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
             Terminal terminal = terminalFactory.createTerminal();
             screen = new TerminalScreen(terminal);
 
@@ -21,25 +43,35 @@ public class Game {
             screen.startScreen();             // screens must be started
             screen.doResizeIfNecessary();     // resize screen if necessary
             screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
             screen.refresh();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void draw(){
+    private void draw() {
         try {
             screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
             screen.refresh();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void run(){
-        draw();
+    public void run() {
+        KeyStroke key = new KeyStroke('q', false, false);
+        do {
+            try {
+                draw();
+                key = screen.readInput();
+                processKey(key);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } while (key.getKeyType() != KeyType.EOF ||
+                (key.getKeyType() == KeyType.Character && key.getCharacter() != 'q'));
+
     }
 }
